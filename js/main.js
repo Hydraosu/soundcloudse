a6b3ea714e75bec8fdd9912a198bf82e6c9fe62d
hydraosu <unjumper@gmail.com>
2017-07-30 20:20:44 +0200

    Fixed image placeholder

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $('document').ready(function() {
+  $('div.addon').hide();
   $('input#search').on('keyup', function(event) {
     if (event.keyCode == 13) { // 13 = Enter Key
       $('button3').click();
@@ -6,17 +7,16 @@ $('document').ready(function() {
   });
 
   function createListItem(item, item2, item3) {
-    var li = $('<li>');
+    var li = $('<li class="clearfix">');
     li.append('<h2>' + item + '</h2>');
     if (!item2) {
 
-      li.append('Brak obrazka! <br>');
+      li.append('<img class="round" src="img/no-image.png" /> <br>');
     } else {
-      li.append('<img src="' + item2 + '" > <br>');
+      li.append('<img class="round" src="' + item2 + '" ><br> ');
     }
-    li.append('<a href="' + item3 + '?client_id=a281614d7f34dc30b665dfcaa3ed7505' + 'class="sc-player"> Download </a>');
+    li.append('<a href="' + item3 + '?client_id=a281614d7f34dc30b665dfcaa3ed7505'+ '" download="song"> Download </a>');
     return li;
-
   }
   $('input#search').on('keyup', function(event) {
     if (event.keyCode == 13) { // 13 = Enter Key
@@ -39,7 +39,7 @@ $('document').ready(function() {
           var title = data[i].title;
           var img = data[i].artwork_url;
           var download = data[i].stream_url;
-
+          $('div.addon').show();
           $('ul').append(createListItem(title, img, download));
         }
 
