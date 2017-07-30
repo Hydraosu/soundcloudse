3b0be4ec41d7abc38bad5ba4da9d9e4514256a14
hydraosu <unjumper@gmail.com>
2017-07-30 02:58:40 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$('document').ready(function() {
+  $('input#search').on('keyup', function(event) {
+    if (event.keyCode == 13) { // 13 = Enter Key
+      $('button3').click();
+    }
+  });
+
+  function createListItem(item, item2, item3) {
+    var li = $('<li>');
+    li.append('<h2>' + item + '</h2>');
+    if (!item2) {
+
+      li.append('Brak jebanego obrazka! <br>');
+    } else {
+      li.append('<img src="' + item2 + '" > <br>');
+    }
+    li.append('<a href="' + item3 + '?client_id=f17c1d67b83c86194fad2b1948061c9e' + '" > Download </a>');
+    return li;
+
+  }
+  $('input#search').on('keyup', function(event) {
+    if (event.keyCode == 13) { // 13 = Enter Key
+      $('button').click();
+    }
+  });
+  $('button').click(function() {
+    var track = $('input#search').val();
+
+    $.ajax({
+      url: 'http://api.soundcloud.com/tracks?client_id=f17c1d67b83c86194fad2b1948061c9e&q=' + track,
+      data: {
+        format: 'json'
+      },
+
+      success: function(data) {
+        $('ul').empty();
+
+        for (var i = 0; i < data.length; i++) {
+          var title = data[i].title;
+          var img = data[i].artwork_url;
+          var download = data[i].stream_url;
+          $('ul').append(createListItem(title, img, download));
+        }
+
+      }
+    });
+  });
+});
